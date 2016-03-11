95729938da3161e91ef0c6f81b2b445d3f8ef961
unknown <MSD010063@MSD010063TSV.hcb-group.local>
2016-03-11 10:07:20 +0900

    first component.

---
+++ src/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<h1>Hello, world!</h1>
+		);
+	}
+}
+
+ReactDOM.render(
+	<App />, 
+	document.getElementById("app")
+);
