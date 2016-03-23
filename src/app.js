0cb92a9e2e79e70e72cba864f4a7df53cbc6b1d4
unknown <MSD010063@MSD010063TSV.hcb-group.local>
2016-03-23 12:47:40 +0900

    remove footer component.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header.react';
-import Footer from './components/Footer.react';
 import Items from './components/Items.react';
 import Item from './components/Item.react';
 import { Router, Route, hashHistory } from 'react-router';
@@ -14,7 +13,6 @@ export default class App extends React.Component {
 			<div>
 				<Header />
 				{this.props.children}
-				<Footer />
 			</div>
 		);
 	}
