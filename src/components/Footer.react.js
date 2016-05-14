54dfc4b6f9f731ddd285ccfceaf1f6db64527782
a_saiki <akio.saiki@gmail.com>
2016-05-15 07:31:09 +0900

    footer fix bottom.

--- src/components/Footer.react.js
+++ src/components/Footer.react.js
@@ -3,10 +3,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const style = {
+	position: "fixed",
+	bottom: "0px",
+	height: "40px",
+	width: "100%",
+	textAlign: "center"
+};
+
 export default class Footer extends React.Component {
 	render() {
 		return (
-			<footer>
+			<footer style={style}>
 				<p>copy right ...</p>
 			</footer>
 		);
